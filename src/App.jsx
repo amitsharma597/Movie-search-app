9459775b73d1bf972b4b4b9f0a3292b235102606
amitsharma597 <amitsharmahsp508@gmail.com>
2026-06-17 12:39:13 +0530

    feat: add navbar and basic layout structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,11 @@
+import Navbar from "./components/Navbar";
+import "./App.css";
 const App = () => {
-  return <div>App</div>;
+  return (
+    <>
+      <Navbar />
+    </>
+  );
 };
 
 export default App;
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,17 @@
+import { Link } from "react-router-dom";
 const Navbar = () => {
-  return <div>Navbar</div>;
+  return (
+    <div>
+      <header>
+        <h1>MovieApp</h1>
+        <nav>
+          <Link to="/">Home</Link>
+          <Link to="/about">About me</Link>
+          <Link to="/">Contact</Link>
+        </nav>
+      </header>
+    </div>
+  );
 };
 
 export default Navbar;
