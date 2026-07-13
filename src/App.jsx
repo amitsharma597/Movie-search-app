bfaa4fd1f374858d3c603fdf61b8c9050334a2f9
amitsharma597 <amitsharmahsp508@gmail.com>
2026-07-13 13:15:33 +0530

    feat:persist search state across routes

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,41 @@ import Favorite from "./pages/Favorite";
 import { useState, useEffect } from "react";
 
 const App = () => {
+  console.log(import.meta.env.VITE_OMDB_API_KEY);
+  const [movies, setMovies] = useState([]);
+  const [query, setQuery] = useState("");
+  const [loading, setloading] = useState(false);
+  const [error, setError] = useState(false);
+  const [empty, setEmpty] = useState("Search for a movie to get started!");
+
+  const fetchMovies = async () => {
+    if (!query.trim()) {
+      setError("Please enter a movie name.");
+      setMovies([]);
+      setEmpty("");
+      return;
+    }
+    setloading(true);
+    setError("");
+    setEmpty("");
+
+    const response = await fetch(
+      `https://www.omdbapi.com/?apikey=${import.meta.env.VITE_OMDB_API_KEY}&s=${query}`,
+    );
+
+    const data = await response.json();
+
+    if (data.Response === "False") {
+      setError(data.Error);
+      setMovies([]);
+      setloading(false);
+    } else {
+      setMovies(data.Search || []);
+      setloading(false);
+    }
+
+    setQuery("");
+  };
   const [favorites, setFavorites] = useState(() => {
     const stored = localStorage.getItem("favorites");
 
@@ -20,7 +55,23 @@ const App = () => {
       <Routes>
         <Route
           path="/"
-          element={<Home favorites={favorites} setFavorites={setFavorites} />}
+          element={
+            <Home
+              favorites={favorites}
+              setFavorites={setFavorites}
+              query={query}
+              setQuery={setQuery}
+              movies={movies}
+              setMovies={setMovies}
+              loading={loading}
+              setloading={setloading}
+              error={error}
+              setError={setError}
+              empty={empty}
+              setEmpty={setEmpty}
+              fetchMovies={fetchMovies}
+            />
+          }
         />
         <Route path="/movie/:imdbID" element={<MovieDetails />} />
         <Route path="/about" element={<About />} />
